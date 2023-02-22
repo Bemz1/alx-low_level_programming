a5629327819fd01c3d93489b419d8454a21de989
Okputu Bemenga Julius <bemiebembem@gmail.com>
2023-02-21 21:03:53 -0800

    Task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
+
 /**
- * main - main function
+ * print_alphabet - function that prints alphabets in lower case
  *
- * print_alphabet: function that prints alphabets in lower case
- *
- * Return: Always 0
+ * Return: Always 0;
  */
 
 void print_alphabet(void)
