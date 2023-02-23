951a561f23e58fb97add5fe4c390b03ff8efbf72
Okputu Bemenga Julius <bemiebembem@gmail.com>
2023-02-23 07:28:01 -0800

    Task2. 0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,17 +10,23 @@
 
 int largest_number(int a, int b, int c)
 {
+
 int largest;
 
 if (a >= b && b >= c)
+
 {
 largest = a;
 }
+
 else if (b >= a && a >= c)
+
 {
 largest = b;
 }
+
 else
+
 {
 largest = c;
 }
