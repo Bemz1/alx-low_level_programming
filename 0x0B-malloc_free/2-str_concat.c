8f8863f0dd6e7f639fe355ed9f5ce57b1beb2c4f
Okputu Bemenga Julius <bemiebembem@gmail.com>
2023-03-13 04:21:18 -0700

    Task2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * _strlen - count array
+ * @s: array of elements
+ * Return: i
+ */
+
+int _strlen(char *s)
+{
+unsigned int i;
+
+i = 0;
+while (s[i] != '\0') /*count character of string*/
+{
+i++;
+}
+
+return (i);
+}
+
+/**
+ * str_concat - back a pointer to an array
+ * @s1: Array one
+ * @s2: Array two
+ * Return: Always an array dynamic
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+char *dest;
+unsigned int i, j, size;
+
+/*if the array is empty*/
+if (s1 == NULL)
+s1 = "";
+
+if (s2 == NULL)
+s2 = "";
+
+/*count size total*/
+size = (_strlen(s1) + _strlen(s2) + 1);
+
+/*malloc*/
+dest = (char *) malloc(size *sizeof(char));
+
+if (dest == 0)
+{
+return (NULL);
+}
+
+/*concatenate arrays*/
+for (i = 0; *(s1 + i) != '\0'; i++)
+*(dest + i) = *(s1 + i);
+
+for (j = 0; *(s2 + j) != '\0'; j++)
+{
+*(dest + i) = *(s2 + j);
+i++;
+}
+return (dest);
+}
